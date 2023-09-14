b8129262affa8bf183d38104eb86093eaaec2b7e
AlexashaSova <sovyshka07@gmail.com>
2023-09-14 23:35:11 +0300

    Commit on run_analysis.R complete and got tidy dataset

--- run_analysis.R
+++ run_analysis.R
@@ -53,4 +53,77 @@ features <- read.table(file.path(paste0(path_to_data, "features.txt")))
 # labels
 labels <- read.table(file.path(paste0(path_to_data, "activity_labels.txt")))
 
+# set column names to the activity labels
+colnames(labels) <- c("activityID",
+                      "label")
+
+
+## STEP-2 - Merging train and test data into one data table
+
+person_activity <- rbind(
+        cbind(train_subj, train_values, train_row_names),
+        cbind(test_subj, test_values, test_row_names)
+)
+rm(train_subj, train_values, train_row_names, test_subj, test_values, test_row_names)
+# setting column names
+colnames(person_activity) <- c("subject", 
+                               features[, 2], 
+                               "activity")
+
+
+## STEP-3 Extracting only the measurements on the mean and std. dev. for each measurement
+
+# choosing columns we need
+needed_cols <- grepl("subject|activity|mean|st_dev", 
+                     colnames(person_activity))
+person_activity <- person_activity[, needed_cols]
+
+
+## STEP-4 Using descriptive activity names to name the activities in the data set
+
+# replace activity values with named factor levels
+person_activity$activity <- factor(person_activity$activity, 
+                                   levels = labels[, 1], 
+                                   labels = labels[, 2])
+
+
+## STEP-5 
+
+# getting and cleaning column names from special symbols, 
+person_activity_colnames <- colnames(person_activity)
+person_activity_colnames <- gsub("[\\(\\)-]", 
+                                 "", 
+                                 person_activity_colnames)
+person_activity_colnames <- gsub("^f", "frequencyDomain", 
+                                 person_activity_colnames)
+person_activity_colnames <- gsub("^t", "timeDomain", 
+                                 person_activity_colnames)
+person_activity_colnames <- gsub("Acc", "Accelerometer", 
+                                 person_activity_colnames)
+person_activity_colnames <- gsub("Gyro", "Gyroscope", 
+                                 person_activity_colnames)
+person_activity_colnames <- gsub("Mag", "Magnitude", 
+                                 person_activity_colnames)
+person_activity_colnames <- gsub("Freq", "Frequency", 
+                                 person_activity_colnames)
+person_activity_colnames <- gsub("mean", "Mean", 
+                                 person_activity_colnames)
+person_activity_colnames <- gsub("std", "StandardDeviation", 
+                                 person_activity_colnames)
+person_activity_colnames <- gsub("BodyBody", "Body", 
+                                 person_activity_colnames)
+# set cleaned labels as a column names
+colnames(person_activity) <- person_activity_colnames
+
+
+## STEP-6 creating second, independent dataset, that is tidy and has an average 
+# of each variable for each activity and each subject
+
+
+person_activity_means <- person_activity %>% group_by(subject, activity) %>% summarise_each(funs(mean))
+# write tidy dataset
+write.table(person_activity_means, 
+            "./out/tidy_dataset.txt", 
+            row.names = FALSE, 
+            quote = FALSE)
 
